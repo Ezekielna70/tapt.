8e446015d2c95efa3d4ee2822b1ca8e0c751fecc
Ezekiel <ewalfredna70@gmail.com>
2026-09-13 17:14:50 +0700

    Initial Commit

---
+++ tapt./tapt_App.swift
@@ -0,0 +1,17 @@
+//
+//  tapt_App.swift
+//  tapt.
+//
+//  Created by Ezekiel Walfred on 13/09/26.
+//
+
+import SwiftUI
+
+@main
+struct tapt_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
